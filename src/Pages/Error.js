4d490056533bb0ada55ce9ecefca363552cdc526
nisabmohd <nisabmohd.com>
2022-02-19 02:35:48 +0530

    Error page commit

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export const Error = () => {
     return (
-        <div>
-            <h1 style={{colour:'red'}}>Error 404  Not Found</h1>
+        <div style={{userSelect:'none'}}>
+            <h1 style={{color:'rgb(25,118,210)',fontSize:'145px',fontFamily:'Roboto'}}>404</h1>
+            <h1 style={{color:'gray',marginTop:'-99px',fontFamily:'Roboto'}}> Not Found</h1>
+            <p style={{color:'gray',fontSize:'19px',fontFamily:'Roboto'}}>The page you were looking for doesn't exist</p>
         </div>
     )
 }
