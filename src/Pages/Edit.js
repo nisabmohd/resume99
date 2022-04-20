943f829a0a9c63a7af35f2ca8a28bf7a5b820c55
nisabmohd <nisabmohd.com>
2022-04-20 21:08:50 +0530

    usr multiple resume handle

--- src/Pages/Edit.js
+++ src/Pages/Edit.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Right } from '../Right'
 import { Preview } from '../Preview'
 import { Leftbar } from '../Leftbar'
 import { LoggedNav } from '../Components/LoggedNav'
+import { useParams } from 'react-router-dom'
+
+
 export const Edit = () => {
+    let params = useParams();
+    useEffect(() => {
+        console.log(params);
+    }, [])
     return (
         <>
             <LoggedNav userimg={JSON.parse(localStorage.getItem('user')).photoURL}></LoggedNav>
-            <div style={{ display: 'flex', flexDirection: 'row' ,width:'91%',margin:'auto'}}>
-                <div className="leftbar" style={{ flex: '0.9', height: '89vh', overflowY: 'scroll' }}>
+            <div style={{ display: 'flex', flexDirection: 'row', width: '91%', margin: 'auto' }}>
+                <div className="leftbar" style={{ flex: '0.9', height: '89vh', overflowY: 'scroll', border: '1px solid red' }}>
                     <Leftbar></Leftbar>
                 </div>
-                <div className="previewbar" style={{ flex: '2', height: '89vh', overflowY: 'scroll' }}>
+                <div className="previewbar" style={{ flex: '2', height: '89vh', overflowY: 'scroll', border: '1px solid red' }}>
                     <Preview></Preview>
                 </div>
-                <div className="rightbar" style={{ flex: 0.9, height: '89vh', overflowY: 'scroll' }}>
+                <div className="rightbar" style={{ flex: 0.9, height: '89vh', overflowY: 'scroll', border: '1px solid red' }}>
                     <Right></Right>
                 </div>
             </div>
