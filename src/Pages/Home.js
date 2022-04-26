a3c2033ee50b59b8a9a3c56f070cb6f2d03b784c
nisabmohd <nisabmohd.com>
2022-04-26 17:22:24 +0530

    signup and sigin using email password and page about added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,39 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AutBtn } from '../Components/AutBtn'
 import { HomeSection } from '../Components/HomeSection'
 import { Navbar } from '../Components/Navbar'
-import github from '../asset/github.png'
+import github from '../asset/login.png'
 import google from '../asset/search.png'
 import bg from '../asset/008-.jpg'
-import { getAuth, signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { googleprovider } from '../config'
-import { githubprovider } from '../config'
-
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 
 
 export const Home = (props) => {
     const auth = getAuth();
-   async function googleLogin() {
-        const user=await signInWithPopup(auth, googleprovider)
-        console.log(user.user);
-        localStorage.setItem('user',JSON.stringify(user.user))
+    async function googleLogin() {
+        const user = await signInWithPopup(auth, googleprovider)
+        localStorage.setItem('user', JSON.stringify(user.user))
         props.setuser(user)
     }
-    async function githubLogin() {
-        const user=await signInWithPopup(auth, githubprovider)
-        console.log(user);
+
+    async function emaillogin(email, password) {
+        const user = await signInWithEmailAndPassword(auth, email, password)
         props.setuser(user)
+        localStorage.setItem('user', JSON.stringify(user.user))
     }
+
+    async function signup(email, password) {
+        const user = await createUserWithEmailAndPassword(auth, email, password)
+        props.setuser(user)
+        localStorage.setItem('user', JSON.stringify(user.user))
+
+    }
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [open1, setOpen1] = React.useState(false);
+    const handleClickOpen1 = () => {
+        setOpen1(true);
+    };
+
+    const handleClose1 = () => {
+        if (!email && !password) return;
+        emaillogin(email, password)
+        setOpen1(false);
+    };
     return (
         <div>
-            <Navbar></Navbar>
+            <Navbar signup={signup}></Navbar>
             <HomeSection></HomeSection>
             <div className="btns" style={{ width: 'fit-content', display: 'flex', flexDirection: 'row', margin: 'auto', marginTop: '45px' }}>
                 <AutBtn name="Login via Google" img={google} col="white" textcol="black" onc={googleLogin}></AutBtn>
-                <AutBtn name="Login via Github" img={github} col="rgb(143 138 138)" textcol="white" onc={githubLogin}></AutBtn>
+                <AutBtn name="resume99 Login" img={github} col="rgb(143 138 138)" textcol="white" onc={handleClickOpen1}></AutBtn>
+                <Dialog open={open1} onClose={handleClose1}>
+                    <DialogTitle>Signin</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Email Address"
+                            type="email"
+                            fullWidth
+                            variant="standard"
+                            required
+                            value={email}
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            sx={{ marginBottom: '16px' }}
+                        />
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Password atleast 6 digits"
+                            type="password"
+                            fullWidth
+                            required
+                            value={password}
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            variant="standard"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose1}>Signin</Button>
+                    </DialogActions>
+                </Dialog>
             </div>
-            <div className="image" style={{width:"100%",display:'flex',justifyContent:'center'}}>
-            <img style={{ width: '650px', margin:'auto',marginTop:'56px',marginBottom:'29px',borderRadius:'10px'}} src={bg} alt="" />
+            <div className="image" style={{ width: "100%", display: 'flex', justifyContent: 'center' }}>
+                <img style={{ width: '650px', margin: 'auto', marginTop: '56px', marginBottom: '29px', borderRadius: '10px' }} src={bg} alt="" />
             </div>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import loginpageimg from '../asset/1.png'
+import templatepage from '../asset/2.png'
+import editpage from '../asset/3.png'
+import editpage1 from '../asset/31.png'
+import editpage2 from '../asset/32.png'
 
-export const Navbar = () => {
+
+export const Navbar = (props) => {
+
+  const [open, setOpen] = React.useState(false);
+  const [open1, setOpen1] = React.useState(false);
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClickOpen1 = () => {
+    setOpen1(true);
+  };
+  const handleClose = () => {
+    if (!email && !password) return;
+    props.signup(email, password)
+    setOpen(false);
+  };
+  const handleClose1 = () => {
+    setOpen1(false);
+  };
+
+
+
   return (
-    <div className='navbar' style={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-between',padding:'0 2vw',marginTop:'-18px'}}>
-        <div className="logo">
-            <p style={{fontWeight:'700',fontSize:'30px',color:'white'}}>resume<span style={{color:'rgb(103,118,237)'}}>99</span></p>
-        </div>
-        <div className="tags">
-            <a href="/" style={{fontFamily:'Helvetica Neue',fontSize:'16px',fontWeight:'700',textDecoration:'none',margin:'0 12px',color:'white'}}>Home</a>
-            <a href="/" style={{fontFamily:'Helvetica Neue',fontSize:'16px',fontWeight:'700',textDecoration:'none',margin:'0 12px',color:'white'}}>About us</a>
-            <a href="/" style={{fontFamily:'Helvetica Neue',fontSize:'16px',fontWeight:'700',textDecoration:'none',margin:'0 12px',color:'white'}}>Login</a>
-        </div>
+    <div className='navbar' style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', padding: '0 2vw', marginTop: '-18px' }}>
+      <div className="logo">
+        <p style={{ fontWeight: '700', fontSize: '30px', color: 'white' }}>resume<span style={{ color: 'rgb(103,118,237)' }}>99</span></p>
+      </div>
+      <div className="tags">
+        <Link to="/" style={{ fontFamily: 'Helvetica Neue', fontSize: '16px', fontWeight: '700', textDecoration: 'none', margin: '0 12px', color: 'white' }}>Home</Link>
+        <button style={{ fontFamily: 'Helvetica Neue', fontSize: '16px', fontWeight: '700', textDecoration: 'none', margin: '0 12px', color: 'white', backgroundColor: 'transparent', outline: 'none', border: 'none', cursor: 'pointer' }} onClick={handleClickOpen1}>About</button>
+        <Dialog maxWidth="xl"   open={open1} onClose={handleClose1}>
+          <DialogTitle>About</DialogTitle>
+          <DialogContent style={{ textAlign: 'left' }}>
+            <p><span style={{ fontWeight: 'bold' }}>resume99</span> is open source resume builder developed bt <span style={{ fontWeight: 'bold' }}><a style={{ color: 'white', textDecoration: 'none' }} href="https://github.com/nisabmohd" target="_blank" rel="noreferrer">Mohd Nisab</a></span></p>
+            <h5>Features : </h5>
+            <li style={{ fontSize: '13px' }}> Save To PDF , Save to JSON , User Friendly , Free forever , No Advertising , No User Tracking , Straight Forward</li>
+            <h5>How to use ?</h5>
+            <li style={{ fontSize: '13px' }}>First Login or signup</li>
+            <img style={{ width: '900px', marginTop: '12px', marginBottom: '12px', borderRadius: '10px'}} src={loginpageimg} alt="" />
+            <li style={{ fontSize: '13px' }}>Select a template or create new</li>
+            <img style={{ width: '900px', marginTop: '12px', marginBottom: '12px', borderRadius: '10px' }} src={templatepage} alt="" />
+            <li style={{ fontSize: '13px' }}>Edit your details</li>
+            <img style={{ width: '900px', marginTop: '12px', marginBottom: '12px', borderRadius: '10px' }} src={editpage} alt="" />
+            <li style={{ fontSize: '13px' }}>Preview your details</li>
+            <img style={{ width: '900px', marginTop: '12px', marginBottom: '12px', borderRadius: '10px' }} src={editpage1} alt="" />
+            <li style={{ fontSize: '13px' }}>Save your resume or change template</li>
+            <img style={{ width: '900px', marginTop: '12px', marginBottom: '12px', borderRadius: '10px' }} src={editpage2} alt="" />
+          </DialogContent>
+        </Dialog>
+        <button style={{ fontFamily: 'Helvetica Neue', fontSize: '16px', fontWeight: '700', textDecoration: 'none', margin: '0 12px', color: 'white', backgroundColor: 'transparent', outline: 'none', border: 'none', cursor: 'pointer' }} onClick={handleClickOpen} >Sign up</button>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Signup</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+              required
+              value={email}
+              onChange={(e) => { setEmail(e.target.value) }}
+              sx={{ marginBottom: '16px' }}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Password atleast 6 digits"
+              type="password"
+              fullWidth
+              required
+              value={password}
+              onChange={(e) => { setPassword(e.target.value) }}
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Signup</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     </div>
   )
 }
