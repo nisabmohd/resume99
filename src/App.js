98cdc93415302fae4829228a1e18961d3570d3e3
nisabmohd <nisabmohd.com>
2022-04-27 14:24:13 +0530

    Pages folder commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
-import { Home } from './pages/Home'
+import { Home } from './Pages/Home'
 import { Dashboard } from './Dashboard'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { LoggedNav } from './Components/LoggedNav';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { Edit } from './pages/Edit';
+import { Edit } from './Pages/Edit';
 
 
 function App() {
